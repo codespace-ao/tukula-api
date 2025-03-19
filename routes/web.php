658290eb540458b122c5b6b997db8ca7d4d0164d
Rinel Benjamim <benjamimrinel192@gmail.com>
2025-03-19 07:01:23 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,18 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    route('/api/v01/docs');
 });
 
 Route::prefix('api/v01')->group(function () {
 
+     Route::get('/docs', function () {
+        return view('welcome');
+    });
+
+    
     Route::prefix('auth')->group(function () {
         # OAUTH2    
         Route::get('/google', [LoginController::class, 'redirectToGoogle'])->name('auth.google');
